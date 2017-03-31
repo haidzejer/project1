ef519d0f209c8bd07951b81630aa04d707ae2ff3
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-03-30 18:22:47 -0700

    added array of cards

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
 console.log('Scripts connected!');
+
+
+var cards = [
+  ['Ace', 'Spades', 11, 0],
+  ['King', 'Spades', 10, -1],
+  ['Queen', 'Spades', 10, -1],
+  ['Jack', 'Spades', 10, -1],
+  ['Ten', 'Spades', 10, -1],
+  ['Nine', 'Spades', 9, 0],
+  ['Eight', 'Spades', 8, 0],
+  ['Seven', 'Spades', 7, 0],
+  ['Six', 'Spades', 6, 1],
+  ['Five', 'Spades', 5, 1],
+  ['Four', 'Spades', 4, 1],
+  ['Three', 'Spades', 3, 1],
+  ['Two', 'Spades', 2, 1],
+  ['Ace', 'Clubs', 11, 0],
+  ['King', 'Clubs', 10, -1],
+  ['Queen', 'Clubs', 10, -1],
+  ['Jack', 'Clubs', 10, -1],
+  ['Ten', 'Clubs', 10, -1],
+  ['Nine', 'Clubs', 9, 0],
+  ['Eight', 'Clubs', 8, 0],
+  ['Seven', 'Clubs', 7, 0],
+  ['Six', 'Clubs', 6, 1],
+  ['Five', 'Clubs', 5, 1],
+  ['Four', 'Clubs', 4, 1],
+  ['Three', 'Clubs', 3, 1],
+  ['Two', 'Clubs', 2, 1],
+  ['Ace', 'Hearts', 11, 0],
+  ['King', 'Hearts', 10, -1],
+  ['Queen', 'Hearts', 10, -1],
+  ['Jack', 'Hearts', 10, -1],
+  ['Ten', 'Hearts', 10, -1],
+  ['Nine', 'Hearts', 9, 0],
+  ['Eight', 'Hearts', 8, 0],
+  ['Seven', 'Hearts', 7, 0],
+  ['Six', 'Hearts', 6, 1],
+  ['Five', 'Hearts', 5, 1],
+  ['Four', 'Hearts', 4, 1],
+  ['Three', 'Hearts', 3, 1],
+  ['Two', 'Hearts', 2, 1],
+  ['Ace', 'Diamonds', 11, 0],
+  ['King', 'Diamonds', 10, -1],
+  ['Queen', 'Diamonds', 10, -1],
+  ['Jack', 'Diamonds', 10, -1],
+  ['Ten', 'Diamonds', 10, -1],
+  ['Nine', 'Diamonds', 9, 0],
+  ['Eight', 'Diamonds', 8, 0],
+  ['Seven', 'Diamonds', 7, 0],
+  ['Six', 'Diamonds', 6, 1],
+  ['Five', 'Diamonds', 5, 1],
+  ['Four', 'Diamonds', 4, 1],
+  ['Three', 'Diamonds', 3, 1],
+  ['Two', 'Diamonds', 2, 1]
+]
+
+
+
+
+
+    //Change the setup to have an input
+    	//once the correct current count is entered, the cards with change
+    		//see how many pairs you can get thru in 20 seconds and compare it the the next player

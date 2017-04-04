697e782713ad2dab37f918fbf269cf28da88ec44
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-04-04 09:10:45 -0700

    completed player one functions

--- app.js
+++ app.js
@@ -1,65 +1,61 @@
-console.log('Scripts connected!');
-
-
 var game = {
   player1: {score: 0},
   player2: {score: 0},
   deck: [
-    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_spades.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_spades.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_spades.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_spades.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_spades.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_spades.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_spades.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_spades.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_spades.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_spades.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_spades.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_spades.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_spades.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_clubs.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_clubs.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_clubs.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_clubs.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_clubs.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_clubs.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_clubs.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_clubs.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_clubs.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_clubs.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_clubs.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_clubs.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_clubs.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_diamonds.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_diamonds.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_diamonds.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_diamonds.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_diamonds.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_diamonds.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_diamonds.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_diamonds.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_diamonds.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_diamonds.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_diamonds.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_diamonds.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_diamonds.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_hearts.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_hearts.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_hearts.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_hearts.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_hearts.png', value: -1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_hearts.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_hearts.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_hearts.png', value: 0},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_hearts.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_hearts.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_hearts.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_hearts.png', value: 1},
-    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_hearts.png', value: 1}
+    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_spades.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_spades.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_spades.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_spades.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_spades.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_spades.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_spades.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_spades.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_spades.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_spades.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_spades.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_spades.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_spades.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_clubs.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_clubs.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_clubs.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_clubs.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_clubs.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_clubs.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_clubs.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_clubs.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_clubs.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_clubs.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_clubs.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_clubs.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_clubs.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_diamonds.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_diamonds.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_diamonds.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_diamonds.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_diamonds.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_diamonds.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_diamonds.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_diamonds.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_diamonds.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_diamonds.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_diamonds.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_diamonds.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_diamonds.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/ace_of_hearts.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/king_of_hearts.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/queen_of_hearts.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/jack_of_hearts.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/10_of_hearts.png', count: '-1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/9_of_hearts.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/8_of_hearts.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/7_of_hearts.png', count: '0'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/6_of_hearts.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/5_of_hearts.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/4_of_hearts.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/3_of_hearts.png', count: '1'},
+    {card: 'images/Playing_Cards/PNG-cards-1.3/2_of_hearts.png', count: '0'}
   ]
 }
-
 
 
 function shuffle(array) {
@@ -80,14 +76,123 @@ function shuffle(array) {
   return array;
 }
 shuffle(game.deck)
-// var currentCard = game.deck.pop().card;
 
-$('#next').on('click', function() {
-  $("#card1").html("<img src=" + game.deck.pop().card + '>');
-  $("#card2").html("<img src=" + game.deck.pop().card + '>');
+
+var $start = $('#start');
+var $p2Start = $('#p2Start');
+var $timer = $('.timer');
+var count = 30;
+var $guess = $('#countGuess');
+var actualCount = 0;
+var wrongCount = 0;
+var $score = $('#score');
+var $p1Overall = $('#p1Overall')
+var $p2Overall = $('#p2Overall')
+var pairs = 0;
+var $winner = $('#winner');
+var winnerPick = '';
+var $p1FinalScore = $('#playerOneFinalScore');
+$timer.text('Time remaining: ' + count);
+// $score.text('Score :' + pairs);
+$p1FinalScore.text('You scored ' + game.player1.score + ' points!');
+$winner.text('Winner: '+ winnerPick)
+$p1Overall.text(game.player1.score);
+$p2Overall.text(game.player2.score);
+
+function displayCards() {
+  var newCard1 = game.deck.pop();
+  var newCard2 = game.deck.pop();
+  actualCount += Number(newCard1.count);
+  actualCount += Number(newCard2.count);
+  $("#card1").html("<img src=" + newCard1.card + '>');
+  $("#card2").html("<img src=" + newCard2.card + '>');
+}
+
+
+$start.on('click', function() {
+  displayCards();
+  $start.css('display', 'none');
+
+  $guess.on('change', function() {
+  	currentGuess = $guess.val()
+    if(currentGuess == actualCount) {
+      displayCards();
+      game.player1.score += 1;
+      pairs ++
+      $score.text('Score :' + pairs);
+      $p1FinalScore.text('You scored ' + game.player1.score + ' points!');
+      $p1Overall.text(game.player1.score);
+      $guess.val('')
+      $('#wrong').html('');
+    }
+    else {
+      $('#wrong').html('<p>Wrong Count, try again!</p>');
+      wrongCount += 1;
+    }
+  })
+
+  var counter = setInterval(timer, 1000);
+
+  function timer()
+  {
+    count = count - 1;
+    $timer.text('Time remaining: ' + count);
+    if (count <= 0)
+    {
+       clearInterval(counter);
+       $('#playerOneTimeOut').css('display', 'block')
+    }
+  }
 });
 
+$p2Start.on('click', function () {
+  $('#playerOneTimeOut').css('display', 'none')
+  count = 30;
+  actualCount = 0;
+  wrongCount = 0;
+  pairs = 0;
+  displayCards();
 
-// console.log([game.deck.pop(), game.deck.pop()])
-// console.log([game.deck.pop(), game.deck.pop()])
-// console.log(game.deck)
+  $guess.on('change', function() {
+    currentGuess = $guess.val()
+    if(currentGuess == actualCount) {
+      displayCards();
+      game.player2.score += 1;
+      pairs ++
+      $score.text('Score :' + pairs);
+      $p2FinalScore.text('You scored ' + game.player2.score + ' points!');
+      $p2Overall.text(game.player2.score);
+      $guess.val('')
+      $('#wrong').html('');
+    }
+    else {
+      $('#wrong').html('<p>Wrong Count, try again!</p>');
+      wrongCount += 1;
+    }
+  })
+
+
+  var counter = setInterval(timer, 1000);
+
+  function timer()
+  {
+    count = count - 1;
+    $timer.text('Time remaining: ' + count);
+    if (count <= 0)
+    {
+       clearInterval(counter);
+       $('#playerTwoTimeOut').css('display', 'block')
+       if(game.player1.score > game.player2.score) {
+         winnerPick = 'Player One'
+         $winner.text('Winner: '+ winnerPick)
+       }
+       else if (game.player1.score < game.player2.score) {
+         winnerPick = 'Player Two'
+         $winner.text('Winner: '+ winnerPick)
+       }
+       else {
+         $('#winner').text('Tie Game!');
+       }
+    }
+  }
+});
